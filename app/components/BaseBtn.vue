16ed5862f920df321cf610749ecddf02e9149d8c
sfauzii <sjahrulfauzii@gmail.com>
2025-08-06 22:58:35 +0700

    refactor: :recycle: project clean up

---
+++ app/components/BaseBtn.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+defineProps<{
+    label?: string;
+    to?: string;
+
+}>();
+
+</script>
+
+<template>
+    <NuxtLink :to="to"
+        class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer">
+        <slot>{{ label }}</slot>
+    </NuxtLink>
+</template>
