5424f365625f5ab8ba03cd37cee9add050f5871f
sfauzii <sjahrulfauzii@gmail.com>
2025-08-06 21:45:13 +0700

    refactor: :recycle: nuxt img

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -6,7 +6,7 @@
     <header class="py-4 border-b">
         <nav class="flex container items-center">
             <NuxtLink href="/" class="flex gap-1 items-center">
-                <img class="w-14" src="/icon-green.png" alt="" />
+                <NuxtImg width="56px" densities="x1" format="webp" class="w-14" src="/icon-green.png" alt="" />
                 <span class="text-3xl font-bold">Nuxtcipes</span>
             </NuxtLink>
             <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
